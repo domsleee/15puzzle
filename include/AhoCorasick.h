ca5298be42f05424a21b26dc877c61f2b1f8feec
Dom Slee <domslee1@gmail.com>
2021-12-27 19:00:05 +1100

    Use better FSM structure

--- include/AhoCorasick.h
+++ include/AhoCorasick.h
@@ -3,10 +3,13 @@
 #include <unordered_set>
 #include <string>
 
+#define AHO_DEBUG(x)
+#define G(i) outAndG[i].second
+
 struct StateMachine {
-    std::vector<int> out, f;
-    std::vector<std::vector<int>> g;
+    std::vector<int> f;
     int state, states;
+    std::vector<std::pair<int, std::vector<int>>> outAndG;
 
     StateMachine(std::vector<std::vector<int>> &&g, std::vector<int> &&out, std::vector<int> &&f, int states);
 
@@ -22,3 +25,24 @@ private:
 };
 
 StateMachine BuildFSMFromStrings(const std::unordered_set<std::string> &strings);
+
+inline int StateMachine::applyMove(int i) {
+    auto oldState = state;
+    state = findNextStateUnsafe(state, i);
+    AHO_DEBUG("apply move " << oldState << " => " << state);
+    return oldState;
+}
+
+inline void StateMachine::undoMove(int state) {
+    this->state = state;
+    AHO_DEBUG("undo move " << state);
+}
+
+inline bool StateMachine::canMove(int i) const {
+    auto nextState = findNextStateUnsafe(state, i);
+    return !outAndG[nextState].first;
+}
+
+inline int StateMachine::findNextStateUnsafe(int state, int i) const {
+    return G(state)[i];
+}
--- src/AhoCorasick.cpp
+++ src/AhoCorasick.cpp
@@ -2,43 +2,24 @@
 #include "../include/AhoCorasick.h"
 #include "../include/Util.h"
 
-#define AHO_DEBUG(x) DEBUG(x)
-#define SM_DEBUG(x)
 
 StateMachine::StateMachine(std::vector<std::vector<int>> &&g, std::vector<int> &&out, std::vector<int> &&f, int states)
-    : g(g),
-      out(out),
-      f(f),
+    : f(f),
       state(0),
-      states(states) {}
-
-int StateMachine::applyMove(int i) {
-    auto oldState = state;
-    state = findNextStateUnsafe(state, i);
-    SM_DEBUG("apply move " << oldState << " => " << state);
-    return oldState;
-}
-
-void StateMachine::undoMove(int state) {
-    this->state = state;
-    SM_DEBUG("undo move " << state);
-}
-
-bool StateMachine::canMove(int i) const {
-    auto nextState = findNextStateUnsafe(state, i);
-    return !out[nextState];
-}
-
-int StateMachine::findNextStateUnsafe(int state, int i) const {
-    return g[state][i];
-}
+      states(states) {
+        outAndG.assign(states, {0, {}});
+        for (auto i = 0; i < states; ++i) {
+            outAndG[i].first = out[i];
+            G(i) = std::move(g[i]);
+        }
+      }
 
 void StateMachine::calcAndSaveNextState(int state, int i) {
-    if (g[state][i] != -1) return;
+    if (G(state)[i] != -1) return;
     auto answer = state;  
-    while (g[answer][i] == -1)
+    while (G(answer)[i] == -1)
         answer = f[answer];
-    g[state][i] = g[answer][i];
+    G(state)[i] = G(answer)[i];
 }
 
 StateMachine BuildFSMFromStrings(const std::unordered_set<std::string> &strings) {
@@ -56,16 +37,13 @@ StateMachine BuildFSMFromStrings(const std::unordered_set<std::string> &strings)
             auto index = static_cast<int>(charToDirection(c));
             if (g[currentState][index] == -1) g[currentState][index] = states++;
             currentState = g[currentState][index];
-            //AHO_DEBUG(string << "[] = " << currentState);
+            AHO_DEBUG(string << "[] = " << currentState);
         }
 
         out[currentState] |= 1;
         i++;
     }
-    AHO_DEBUG("STATES: " << states << ", numNodes " << numNodes);
-    out.resize(states);
-    f.resize(states);
-    g.resize(states);
+    DEBUG("STATES: " << states << ", numNodes " << numNodes);
     for (auto i = 0; i < 4; ++i) {
         if (g[0][i] == -1) g[0][i] = 0;
     }
@@ -94,12 +72,11 @@ StateMachine BuildFSMFromStrings(const std::unordered_set<std::string> &strings)
     }
 
     for (auto i = 0; i < states; ++i) {
-        //AHO_DEBUG("out[" << i << "] = " << out[i]);
+        AHO_DEBUG("out[" << i << "] = " << out[i]);
     }
     for (auto i = 0; i < states; ++i) {
-        //AHO_DEBUG("f[" << i << "] = " << f[i]);
+        AHO_DEBUG("f[" << i << "] = " << f[i]);
     }
-    AHO_DEBUG("Precomputing all states for FSM...");
 
     auto fsm = StateMachine(std::move(g), std::move(out), std::move(f), states);
     for (int state = 0; state < states; ++state) {
